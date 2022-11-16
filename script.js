85fe31a6dc9a3234a08be84f5a2110e9283120be
JamesLyfe <jameshoracephillips@gmail.com>
2022-11-16 11:37:18 -0500

    added reset button
    added reset button

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ function calculateTip() {
     let total = (billAmt * serviceQual) / numOfPeople;
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2);
-    
-    
+
     //Display the tip
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;}
     document.getElementById("calculate").onclick = function() {calculateTip();};
+
+function reset() {
+    document.getElementById("total").value = "";
+    document.getElementById("serviceQual").value = "";
+    document.getElementById("peopleamt").value = "";
+    document.getElementById("tip").innerHTML = "0.00";
+}
+
+    //reset button
+    document.getElementById("reset").onclick = function() {calculateTip();};
